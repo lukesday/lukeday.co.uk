4f530deca2a16f693757c38e11350e2df0865aac
Luke Day <puke929@gmail.com>
2017-12-19 16:51:49 +0000

    Moving social links to an inline react component

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -1,18 +1,22 @@
 import React from "react";
 
-const social = [
+const links = [
     {name: "GitHub", link: "https://github.com/lukesday"},
 ]
+
+const Social = () => (
+    <ul>
+    {links.map((entry) => 
+        <li><a href={entry.link}>{entry.name}</a></li>
+    )}
+    </ul>
+)
 
 export default ({ children, data }) => (
   <div className="footer">
     <div className="container">
         <h3>Follow Me</h3>
-        <ul>
-        {social.map((entry) => 
-            <li><a href={entry.link}>{entry.name}</a></li>
-        )}
-        </ul>
+        <Social />
     </div>
   </div>
 );
